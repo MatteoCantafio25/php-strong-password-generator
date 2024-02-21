d70f609f90b490f13f902be73288d645555171be
MatteoCantafio25 <mattyteo004@gmail.com>
2024-02-21 15:58:49 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Matteo Cantafio">
+    <meta name="description" content="PHP Hotel">
+    <title>PHP Strong Password Generator</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous'/>
+</head>
+<body>
+    
+</body>
+</html>
